64832a89aa5d088ce95b2791cc867ab3968f5daf
Prateek Shekhawat <42514381+shekhawatprateek@users.noreply.github.com>
2021-05-16 23:08:49 +0530

    first commit

---
+++ build/precache-manifest.059a9ba9cf7aa20502a8a3282e538dbc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d4d910a882aea217226db8f41fc342",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb7bd3f7bf14631d295a",
+    "url": "/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "d4cac8143fd0013002b4",
+    "url": "/static/css/main.16516185.chunk.css"
+  },
+  {
+    "revision": "cb7bd3f7bf14631d295a",
+    "url": "/static/js/2.717f1fdb.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/static/js/2.717f1fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4cac8143fd0013002b4",
+    "url": "/static/js/main.587e90f3.chunk.js"
+  },
+  {
+    "revision": "60949e83de0aeb529ab1",
+    "url": "/static/js/runtime-main.25c7e1d9.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/static/media/software.43049cae.jpeg"
+  }
+]);
